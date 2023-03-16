215fb37280f4da9f6245b7dcf313480df8bc3d73
Will Woods Ballard <willwoodsb@gmail.com>
2023-03-16 13:30:47 +0000

    added some formatting to nav bar

--- photo-gallery/resources/views/components/layout.blade.php
+++ photo-gallery/resources/views/components/layout.blade.php
@@ -9,21 +9,44 @@
 <link rel="stylesheet" href="{{ asset('css/application.css') }}">
 
 
-<body class="bg-gray-100">
+<body class="bg-black">
     <header>
-        <nav class="flex flex-row justify-center mx-auto bg-black">
-            <x-nav-item href="/">Home</x-nav-item>
-            @foreach(App\Models\Category::all('name', 'slug') as $cat)
+        <nav class="flex flex-row justify-center mx-auto bg-black mt-4">
+            <div class="nav-item">
+                <a href="/" class="flex flex-col justify-center w-32 font-semibold text-sm text-center text-white hover:bg-gray-100 hover:text-black h-16 py-2">
+                    <div class="uppercase p-2">
+                        Home
+                    </div>
+                    <div class="dropdown">
 
-                <x-nav-item 
-                    href="/category/{{ $cat->slug }}" 
-                    :category="$category" 
-                >
-
-                    {{ $cat->name }}
-
-                </x-nav-item>
-                
+                    </div>
+                </a>
+            </div>
+            
+            @foreach(App\Models\Category::all() as $cat)
+                @if ($cat->id == $category->id)
+                    <div class="nav-item active font-semibold text-sm text-center hover:bg-gray-100 text-white hover:text-black">
+                @else 
+                    <div class="nav-item font-semibold text-sm text-center hover:bg-gray-100 text-white hover:text-black">
+                @endif
+                    <a href="/category/{{ $cat->slug }}" class="flex flex-col justify-center w-32 h-16 py-2">
+    
+                        <div class="p-2 uppercase">
+                            {{ $cat->name }}
+                        </div>
+                    </a>
+                    <div class="dropdown px-3 w-full bg-gray-100">
+                        <div>
+                            @foreach($cat->subCategories as $sub)
+                                <p class="py-2 font-semibold hover:underline">
+                                    <a href="/categories/{{$cat->slug}}/{{$sub->slug}}" >
+                                        {{ ucwords($sub->name) }}
+                                    </a>
+                                </p>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
             @endforeach
         </nav>
     </header>
